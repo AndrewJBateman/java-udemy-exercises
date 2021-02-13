ce52efa901b8af4cbb2356b642cb208ae95a6a15
AndrewJBateman <gomezbateman@yahoo.com>
2021-02-13 22:32:37 +0100

    exercises up to 38 completed

--- src/Main.java
+++ src/Main.java
@@ -127,19 +127,26 @@ public class Main {
 //        VipCustomer person3 = new VipCustomer("Mickey", 100.00, "noddy1@email.com");
 //        System.out.println(person3.getName());
 //        System.out.println(person3.getEmailAddress());
-        Wall wall1 = new Wall("West");
-        Wall wall2 = new Wall("East");
-        Wall wall3 = new Wall("South");
-        Wall wall4 = new Wall("North");
+//        Wall wall1 = new Wall("West");
+//        Wall wall2 = new Wall("East");
+//        Wall wall3 = new Wall("South");
+//        Wall wall4 = new Wall("North");
+//
+//        Ceiling ceiling = new Ceiling(12,55);
+//
+//        Bed bed = new Bed("Modern",4,3,2,1);
+//        Lamp lamp = new Lamp("Classic",false,75);
+//
+//        Bedroom bedroom = new Bedroom("Samuel",wall1,wall2,wall3,wall4,ceiling,bed,lamp);
+//        bedroom.makeBed();
+//
+//        bedroom.getLamp().turnOn();
 
-        Ceiling ceiling = new Ceiling(12,55);
-
-        Bed bed = new Bed("Modern",4,3,2,1);
-        Lamp lamp = new Lamp("Classic",false,75);
-
-        Bedroom bedroom = new Bedroom("Samuel",wall1,wall2,wall3,wall4,ceiling,bed,lamp);
-        bedroom.makeBed();
-
-        bedroom.getLamp().turnOn();
+        Printer printer = new Printer(50, true);
+        System.out.println("initial page count = " + printer.getPagesPrinted());
+        int pagesPrinted = printer.printPages(4);
+        System.out.println("Pages printed was " + pagesPrinted +" new total print count for printer = " + printer.getPagesPrinted());
+        pagesPrinted = printer.printPages(2);
+        System.out.println("Pages printed was " + pagesPrinted +" new total print count for printer = " + printer.getPagesPrinted());
     }
 }
---
+++ src/Printer.java
@@ -0,0 +1,37 @@
+public class Printer {
+    private int tonerLevel;
+    private int pagesPrinted;
+    private boolean duplex;
+
+    public Printer(int tonerLevel, boolean duplex) {
+        this.tonerLevel = tonerLevel > -1 ? tonerLevel : -1;
+        this.duplex = duplex;
+        this.pagesPrinted = 0;
+    }
+
+    public int addToner(int tonerAmount) {
+        if(tonerAmount <= 0 || tonerAmount > 100) return -1;
+
+        if(this.tonerLevel + tonerAmount > 100) {
+            return -1;
+        }
+        return this.tonerLevel += tonerAmount;
+    }
+
+    public int printPages(int pages) {
+        int pagesToPrint;
+
+        if(duplex) {
+            pagesToPrint = pages / 2 + pages % 2;
+            System.out.println("Printing in duplex mode");
+        } else {
+            pagesToPrint = pages;
+        }
+        this.pagesPrinted += pagesToPrint;
+        return pagesToPrint;
+    }
+
+    public int getPagesPrinted() {
+        return pagesPrinted;
+    }
+}
